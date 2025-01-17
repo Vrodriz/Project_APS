6c97611f2ce2185b0318a0fa29e60942c5995aa0
Vrodriz <vrfern0ndes@gmail.com>
2025-01-17 10:16:08 -0400

    :sparkles:  add dropdown close functionality when clicking outside

--- src/app/FirstPage/component/Abrangências/Abrangencias.tsx
+++ src/app/FirstPage/component/Abrangências/Abrangencias.tsx
@@ -8,7 +8,7 @@ export default function Coverage() {
       <div className="flex flex-row gap-4 py-2 justify-center items-center">
       <ScopeControl label="Recursos" options={["TF-1", "TF-2", "TF-3", "TF-4"]} />
       <ScopeControl label="Turnos" options={["Turno 1", "Turno 2", "Turno 3"]} />
-      <ScopeControl label="O.Ps" options={["OP-1", "OP-2", "OP-3"]} />
+      <ScopeControl label="O.Ps" options={["OP-1", "OP-2", "OP-3", "OP-4"]} />
 
       </div>
     </Card>
