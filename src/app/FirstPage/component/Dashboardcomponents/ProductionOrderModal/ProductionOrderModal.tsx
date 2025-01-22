1411571e5f759bde4bf65587cd644e2496547ea6
Vrodriz <vrfern0ndes@gmail.com>
2025-01-22 18:00:16 -0400

    updates

--- src/app/FirstPage/component/Dashboardcomponents/ProductionOrderModal/ProductionOrderModal.tsx
+++ src/app/FirstPage/component/Dashboardcomponents/ProductionOrderModal/ProductionOrderModal.tsx
@@ -13,3 +13,21 @@ interface ProductionOrderModalProps {
         endDate: string;
 }) => void;
 }
+
+const ProductionOrderModalProps: React.FC<ProductionOrderModalProps> = ({
+    isOpen,
+    onClose,
+    onSave,
+}) => { 
+
+    const [machine, setMachine] = React.useState('');
+    const [product, setProduct] = React.useState('');
+    const [shifts, setShifts] = React.useState(1);
+    const [startDate, setStartDate] = React.useState('');   
+    const [endDate, setEndDate] = React.useState('');
+
+    const handleSave = () => {  
+        
+    }
+
+}
