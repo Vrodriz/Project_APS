65a3777fcdeead18dcd4a52a680d9ff78b53b0a6
Vrodriz <vrfern0ndes@gmail.com>
2025-01-24 10:39:14 -0400

    :sparkles: updates the width tr for responsives

--- src/app/components/ProductionCalendar/ProductionCalendar.tsx
+++ src/app/components/ProductionCalendar/ProductionCalendar.tsx
@@ -1,40 +1,16 @@
 'use client'
 
 import React, { useState, useEffect, JSX } from 'react';
-import { MachineCalendar }  from './MachineCalendar';
+import { MachineCalendar } from './MachineCalendar';
 
 const ProductionCalendar = () => {
   const [productionCalendar, setProductionCalendar] = useState([
-    {
-      c_recurso: "TF-1",
-      dataProg: "2025-01-01",
-      turno: 1,
-    },
-    {
-      c_recurso: "TF-1",
-      dataProg: "2025-01-01",
-      turno: 2,
-    },
-    {
-      c_recurso: "TF-1",
-      dataProg: "2025-01-01",
-      turno: 3,
-    },
-    {
-      c_recurso: "TF-1",
-      dataProg: "2025-01-02",
-      turno: 1,
-    },
-    {
-      c_recurso: "TF-1",
-      dataProg: "2025-01-02",
-      turno: 2,
-    },
-    {
-      c_recurso: "TF-1",
-      dataProg: "2025-01-02",
-      turno: 3,
-    },
+    { c_recurso: "TF-1", dataProg: "2025-01-01", turno: 1 },
+    { c_recurso: "TF-1", dataProg: "2025-01-01", turno: 2 },
+    { c_recurso: "TF-1", dataProg: "2025-01-01", turno: 3 },
+    { c_recurso: "TF-1", dataProg: "2025-01-02", turno: 1 },
+    { c_recurso: "TF-1", dataProg: "2025-01-02", turno: 2 },
+    { c_recurso: "TF-1", dataProg: "2025-01-02", turno: 3 },
   ]);
 
   const [iniper, setIniper] = useState(new Date(Date.UTC(2025, 0, 1)));
@@ -43,12 +19,16 @@ const ProductionCalendar = () => {
 
   useEffect(() => {
     const generateHeaders = (iniper: Date, fimper: Date) => {
-      const headers = [];
+      const headers: JSX.Element[] = [];
       const week = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'];
       for (let data = new Date(iniper); data <= fimper; data.setUTCDate(data.getUTCDate() + 1)) {
         let day = data.getUTCDate() < 10 ? "0" + data.getUTCDate() : data.getUTCDate();
         let dayWeek = week[data.getUTCDay()];
-        headers.push(<th key={data.toISOString()} className="border border-gray-300 px-4 py-2">{day+"/"+dayWeek}</th>);
+        headers.push(
+          <th key={data.toISOString()} className="border-b border-gray-300 p-2 w-[150px] text-xs sm:text-sm md:text-base">
+            {day}/{dayWeek}
+          </th>
+        );
       }
       return headers;
     };
@@ -57,24 +37,29 @@ const ProductionCalendar = () => {
   }, [iniper, fimper]);
 
   return (
-    <div className='w-auto'>
-      <div className="overflow-auto p-10 space-x-0">
-      <table className="border border-gray-300 text-center">
-        <thead className="bg-customBrown text-white">
-          <tr className=']'>
-            <th className="border border-gray-300 px-4 py-2">C. Recurso</th>
-            <th className="border border-gray-300 w-7 px-4 py-2 ">Turno</th>
-            
-          </tr>
-        </thead>
-        <tbody>
-          
-         
-        </tbody>
-      </table>
+    <div className='w-full p-4'>
+      <div className="overflow-x-auto max-w-full max-h-[600px]">
+        <table className="border border-gray-300 text-nowrap w-full border-collapse table-fixed">
+          <thead className="bg-customBrown text-white">
+            <tr>
+              <th className="border-b  border-gray-300 p-2 text-sm w-[150px]">C. Recurso</th>
+              <th className="border-b border-gray-300 p-2 text-sm w-[150px]">Turno</th>
+              {headers}
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {productionCalendar.map((production) => (
+              <MachineCalendar
+                key={`${production.c_recurso}-${production.dataProg}-${production.turno}`}
+                machine={production.c_recurso}
+                iniper={iniper}
+                fimper={fimper}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-    </div>
-    
   );
 };
 
